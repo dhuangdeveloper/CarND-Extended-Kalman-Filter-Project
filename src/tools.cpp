9627c3b9011fd63f8520cff2b6031a5c28818d5f
Dayu Huang <dhuang0413@gmail.com>
2018-01-21 19:20:38 +0000

    Reuse the code written during quiz for tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,11 +15,60 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  // Calculate the RMSE here
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  // check the validity of the following inputs:
+  // * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  // ... your code here
+  if ( (estimations.size()==0) || (ground_truth.size()==0) || (ground_truth.size()!= estimations.size()))
+  {
+    return rmse;
+  }
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+	  // ... your code here
+	  VectorXd residual(4);
+	  residual = estimations[i] - ground_truth[i];
+	  residual = residual.array()*residual.array(); 
+	  rmse += residual;
+  }
+
+  //calculate the mean
+  // ... your code here
+  rmse = rmse / estimations.size();
+  //calculate the squared root
+  // ... your code here
+  rmse = rmse.array().sqrt();
+  //return the result
+  return rmse;  
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {  
+  //Calculate a Jacobian here.
+  MatrixXd Hj(3,4);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+    
+  float pxy2 = px * px + py * py;
+  float pxy = sqrt(pxy2);
+  float pxy3_2 = pxy * pxy2;
+  //check division by zero 
+  
+  if ( (fabs(px) < 0.0001  && fabs(py) < 0.0001))
+  {
+	  cout << "Error: Both px and py are zero " << endl;
+	  return Hj;
+  }
+  
+  //compute the Jacobian matrix
+  Hj << px / pxy, py / pxy, 0, 0,
+  - py / pxy2, px / pxy2, 0, 0,
+  py * (vx * py - vy * px) / pxy3_2, px * (vy *px - vx * py) / pxy3_2, px / pxy, py / pxy;
+  
+  return Hj;  
 }
